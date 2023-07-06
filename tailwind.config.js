cee1c82446f48ae034dc3f5141e6551a23d8c2d1
ehabhosam <ehab.hosam2019@gmail.com>
2023-07-06 22:19:09 +0300

    initilize tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgba(41, 87, 255, 1)',
+      },
+      fontFamily: {
+        primary: 'Space Grotesk',
+        secondary: 'Poppins',
+      },
+    },
+  },
+  plugins: [],
+}
