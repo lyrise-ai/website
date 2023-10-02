c7dabf01d2dafbc2556a19fa7e69d9558347b9b0
ehabhosam <ehab.hosam2019@gmail.com>
2023-10-02 20:40:05 +0300

    New landing page: data security section mobile version

--- src/components/NewLanding/security.jsx
+++ src/components/NewLanding/security.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import first from '/src/assets/security/1.jpeg'
 import second from '/src/assets/security/2.jpeg'
 import third from '/src/assets/security/3.jpeg'
+import { useMediaQuery } from '@mui/material'
 
 const toBgColor = {
   0: ' bg-[#5277FF]',
@@ -32,8 +33,17 @@ const items = [
   },
 ]
 
+const findElse = {
+  0: [1, 2],
+  1: [0, 2],
+  2: [0, 1],
+}
+
 function Security() {
   const [active, setActive] = useState(0)
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
+  if (isMobile) return <MobileVersion active={active} setActive={setActive} />
 
   return (
     <div className="text-center w-full mt-32">
@@ -108,3 +118,76 @@ function Security() {
 }
 
 export default Security
+
+function MobileVersion({ active, setActive }) {
+  console.log('rendering mobile version', active)
+  return (
+    <div className="text-center w-full mt-32">
+      <h3 className="text-neutral-500 font-secondary mb-3">
+        Data Security & Infrastructure
+      </h3>
+      <h1 className="text-3xl lg:text-4xl max-w-[500px] m-auto font-medium mb-20 font-primary max-sm:max-w-[90%]">
+        Elevate Your Data Security with IBM Hyper Protect Infrastructure
+      </h1>
+      {/* active item */}
+      <div
+        key={active}
+        className={
+          'col-span-2 max-w-[100%]' +
+          ' max-h-[30vh] h-[300px] transition-all rounded-xl max-md:m-3'
+        }
+        onClick={() => setActive(active)}
+      >
+        <div
+          className="grid grid-cols-2 gap-5 rounded-lg border-[4px] border-white bg-[#EFF2FF] p-3 h-full w-full"
+          style={{ boxShadow: '0px 9px 18px 0px rgba(0, 34, 158, 0.15)' }}
+        >
+          <div className="text-left flex-1 flex flex-col justify-between">
+            <div className="text-sm md:text-[1rem] font-semibold font-secondary my-1 lg:my-3">
+              {items[active].title}
+            </div>
+            <div className="text-xs md:text-[1rem] font-secondary text-neutral-500 mb-3">
+              {items[active].subtitle}
+            </div>
+            <button
+              className="bg-blue-500 p-3 py-2 text-[1rem] font-secondary text-white font-bold rounded-md w-fit"
+              type="button"
+              onClick={() => {
+                window.open('https://www.lyrise.ai/Employer', '_blank')
+              }}
+            >
+              Get Started
+            </button>
+          </div>
+          <Image
+            src={items[active].imgSrc}
+            objectFit="cover"
+            // width={280}
+            // height={200}
+            // style={{ flexShrink: 0, flex: 1 }}
+          />
+        </div>
+      </div>
+      {/* other items */}
+      <div className="flex gap-[2vw] m-3">
+        {findElse[active].map((item) => (
+          <div
+            key={item}
+            className={
+              'col-span-1 w-[47vw] h-[47vw] cursor-pointer' +
+              toBgColor[item] +
+              ' max-h-[30vh]  transition-all rounded-xl'
+            }
+            onClick={() => setActive(item)}
+          >
+            <div className="flex items-center justify-center h-full w-full">
+              <p className="text-center text-white font-semibold text-xl font-primary">
+                {items[item].title}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
