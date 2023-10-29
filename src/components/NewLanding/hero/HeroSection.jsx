d4c6cab9710b50a886689dc9c208d1d8f936c432
Ehab Hosam <68334617+ehabhosam@users.noreply.github.com>
2023-10-29 19:30:34 +0200

    typo fix "Business"

--- src/components/NewLanding/hero/HeroSection.jsx
+++ src/components/NewLanding/hero/HeroSection.jsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <div className="flex flex-col-reverse md:grid grid-cols-3 w-full px-10x relative m-auto mx-10x mb-20 overflow-hidden">
       <div className="col-span-1 flex flex-col justify-center max-md:text-center md:ml-10 lg:ml-32 max-md:max-w-[80vw] max-md:m-auto max-md:items-center">
         <h1 className="text-3xl lg:text-5xl font-semibold mb-6 font-primary">
-          Unlock A.I for your Buisness
+          Unlock A.I for your Business
         </h1>
         <h3 className="pr-5 mb-6 text-neutral-600 font-secondary max-md:text-sm max-md:font-semibold lg:font-primary">
           Find the perfect candidate for your team and hire the top AI and data
