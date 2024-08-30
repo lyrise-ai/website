ad546cef2c0882d04bab70a0667d1d7020ae4f40
Ehab Hosam <68334617+ehabhosam@users.noreply.github.com>
2024-08-30 12:41:28 +0300

    Add google ads script

--- pages/_app.js
+++ pages/_app.js
@@ -106,6 +106,19 @@ export default function MyApp(props) {
         async
         src="https://www.googletagmanager.com/gtag/js?id=GTM-MF93SZM"
       />
+      
+      {/* Google Ads Script */}
+      <Script strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-10840230589">
+      </Script>
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        `}
+        gtag('config', 'AW-10840230589');
+      </Script>
+      
       {/* <script type="text/javascript">
         window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
         heap.load("3331170087");
