f9fcd3bc1508adb97b08fb331e2fffd7717457d7
Ehab Hosam <ehab.hosam2019@gmail.com>
2024-09-17 01:34:49 +0300

    fix(rightarrowicon): forgot to add className as a prop

--- src/assets/icons/rightArrowIcon.jsx
+++ src/assets/icons/rightArrowIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function RightArrowIcon() {
+export default function RightArrowIcon({ className }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
