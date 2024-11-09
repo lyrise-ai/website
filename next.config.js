d0d7355668d5f4612e9781f0e24f6b36b9936382
unixehab <ehab.hosam2019@gmail.com>
2024-11-09 18:11:45 +0200

    downgrade back to next 13 due to next 14 errors

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  experimental: {
-    images: {
-      allowFutureImage: true,
-      // domains: ["www.gravatar.com", "www.freepnglogos.com"],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'www.gravatar.com',
-          port: '',
-          pathname: '/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-        },
-        {
-          protocol: 'https',
-          hostname: 'www.freepnglogos.com',
-          port: '',
-          pathname: '/uploads/image-microsoft-logo--5.png',
-        },
-        {
-          protocol: 'https',
-          hostname: 'www.freepnglogos.com',
-          port: '',
-          pathname:
-            '/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png',
-        },
-      ],
-    },
-  },
   reactStrictMode: false,
   webpack: (config) => {
     config.resolve.alias = {
