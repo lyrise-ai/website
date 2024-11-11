cfb8d721e7895608547977308a8dd4789f03c3c6
Ehab Hosam <ehab.hosam2019@gmail.com>
2024-11-11 20:02:39 +0200

    feat: accelerator promo banner & close icon

--- src/components/NewLanding/accelerator-promo.jsx
+++ src/components/NewLanding/accelerator-promo.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react'
 import ArrowButton from '@components/Buttons/ArrowButton'
 import Countdown from '@components/Accelerator/countdown'
 import GiftIcon from '@assets/icons/giftIcon'
+import CloseIcon from '@assets/icons/closeIcon'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import xIconSrc from '@assets/x.svg'
+import bannerSrc from '@assets/top-winners-banner.png'
 
 export default function AcceleratorPromo() {
   const [isOpen, setIsOpen] = useState(false)
@@ -20,10 +22,12 @@ export default function AcceleratorPromo() {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center fade-in-out">
-      <div className="max-w-md mx-auto p-6 md:max-w-2xl lg:max-w-5xl xl:max-w-5xl">
+      <div className="max-w-md mx-auto p-6 md:max-w-2xl">
         <div className="rounded-xl border-2 border-rose-500 p-3 md:p-8 lg:p-10 space-y-3 bg-primary-25 md:space-y-8 font-primary md:flex md:flex-wrap md:justify-between relative">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight my-0 text-rose-500">
-            LyRise&apos;s First AI Accelerator for Startups!
+          <h1 className="text-4xl md:text-5xl lg:text-5xl font-bold leading-tight my-0">
+            LyRise AI
+            <br />
+            Accelerator!
           </h1>
 
           <div className="space-y-4 md:w-full lg:w-3/5 xl:w-2/3">
@@ -37,12 +41,12 @@ export default function AcceleratorPromo() {
           <button
             type="button"
             onClick={() => setIsOpen(false)}
-            className="absolute right-4 -top-2 rounded-sm opacity-70 hover:opacity-100 focus:outline-none"
+            className="absolute right-2 -top-2 rounded-sm opacity-70 hover:opacity-100 focus:outline-none"
           >
-            x<span className="sr-only">Close</span>
+            <CloseIcon className="h-8 w-8" />
           </button>
 
-          <div className="bg-rose-50 rounded-2xl p-4 flex items-center gap-3 md:w-full lg:w-auto">
+          {/* <div className="bg-rose-50 rounded-2xl p-4 flex items-center gap-3 md:w-full lg:w-auto">
             <GiftIcon className="h-16 w-auto text-rose-500" />
             <div>
               <div className="text-rose-500 font-medium text-base md:text-lg font-secondary">
@@ -52,9 +56,11 @@ export default function AcceleratorPromo() {
                 $45,000
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className="md:w-full lg:w-1/3 xl:w-1/4">
+          <Image src={bannerSrc} alt="Top Winners Banner" />
+
+          <div className="w-full">
             <Countdown
               timestamp={new Date('2024-11-16').getTime()}
               withoutWrapper
