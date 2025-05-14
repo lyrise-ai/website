5d9ee847f02bcf0afac4536a9a9172cfed240c89
Amr Tarek <81596628+AmrrTarekk@users.noreply.github.com>
2025-05-14 15:02:47 +0300

    Add new page: pages/testroute-new.js

---
+++ pages/testroute-new.js
@@ -0,0 +1,14 @@
+import Footer from '../src/components/CustomLandingPages/Footer'
+import Header from '../src/components/CustomLandingPages/Header'
+import HireAiPage from '../src/components/CustomLandingPages/HireAiPage'
+import siteContent from '../src/data/testroute-new.json'
+
+export default function TestrouteNew() {
+  return (
+    <main className="w-full min-h-screen h-full relative custom-landing-pages flex flex-col">
+      <Header  siteContent={siteContent}/>
+      <HireAiPage siteContent={siteContent} />
+      <Footer />
+    </main>
+  )
+}
